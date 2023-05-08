b23735487d859b1d1a54583bac314301265c9b4a
Kyaw Thu <evanch98@gmail.com>
2023-05-08 21:27:31 +0800

    import Navbar component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Nunito } from 'next/font/google'
+import Navbar from './components/navbar/Navbar'
 
 const font = Nunito({ subsets: ['latin'] })
 
@@ -15,7 +16,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <Navbar />
+      <body className={font.className}>
+        {children}
+      </body>
     </html>
   )
 }
