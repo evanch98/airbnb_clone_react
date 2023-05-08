f5c7af88c25cc6cc6a6dcac9255cc5a5a1873218
Kyaw Thu <evanch98@gmail.com>
2023-05-08 21:55:02 +0800

    add width and padding appropriately depending on the screen size

--- app/components/Container.tsx
+++ app/components/Container.tsx
@@ -8,7 +8,18 @@ const Container: React.FC<ContainerProps> = ({
   children
 }) => {
   return (
-    <div>{children}</div>
+    <div
+      className="
+        max-w-[2520px]
+        mx-auto
+        xl:px-20
+        md:px-10
+        sm:px-2
+        px-4
+      "
+    >
+      {children}
+    </div>
   );
 }
 
