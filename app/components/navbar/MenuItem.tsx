5cda47eee05638c16105ea293203a475075fb50b
Kyaw Thu <evanch98@gmail.com>
2023-05-09 14:43:06 +0800

    update the MenuItem component

--- app/components/navbar/MenuItem.tsx
+++ app/components/navbar/MenuItem.tsx
@@ -10,7 +10,18 @@ const MenuItem: React.FC<MenuItemProps> = ({
   label
 }) => {
   return ( 
-    <div></div>
+    <div
+      onClick={onClick}
+      className="
+        px-4
+        py-3
+        hover:bg-neutral-100
+        transition
+        font-semibold
+      "
+    >
+      {label}
+    </div>
    );
 }
  
