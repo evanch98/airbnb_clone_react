71b575b79ee3857421bf32a650a42b61f9d3b131
Kyaw Thu <evanch98@gmail.com>
2023-05-09 13:59:26 +0800

    add more contents to UserMenu component

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { AiOutlineMenu } from 'react-icons/ai';
+
 const UserMenu = () => {
   return (
     <div className="relative">
@@ -20,6 +22,26 @@ const UserMenu = () => {
           "
         >
           Airbnb your home
+        </div>
+        <div
+          onClick={() => {}}
+          className="
+          p-4
+          md:py-1
+          md:px-2
+          border-[1px]
+          border-neutral-200
+          flex
+          flex-row
+          items-center
+          gap-3
+          rounded-full
+          cursor-pointer
+          hover:shadow-md
+          transition
+          "
+        >
+          <AiOutlineMenu />
         </div>
       </div>
     </div>
