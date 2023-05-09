8d7e869bd9e6678cefbecc3084d7c9c32a3d65ca
Kyaw Thu <evanch98@gmail.com>
2023-05-09 23:35:24 +0800

    add an interface ButtonProps

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,6 +1,17 @@
+import { IconType } from "react-icons";
+
+interface ButtonProps {
+  label: string,
+  onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  disabled?: boolean;
+  outline?: boolean;
+  small?: boolean;
+  icon?: IconType;
+}
+
 const Button = () => {
   return ( 
-    
+    <button></button>
    );
 }
  
