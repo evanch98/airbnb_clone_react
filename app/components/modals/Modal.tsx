3e46f1a85a8c81d1b2f894ddf8f54ea658e99e01
Kyaw Thu <evanch98@gmail.com>
2023-05-09 17:05:18 +0800

    add ModalProps interface

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -1,3 +1,16 @@
+interface ModalProps {
+  isOpen?: boolean;
+  onClose: () => void;
+  onSubmit: () => void;
+  title?: string;
+  body?: React.ReactElement;
+  footer?: React.ReactElement;
+  actionLabel: string;
+  disabled?: boolean;
+  secondaryAction?: () => void;
+  secondaryLabel?: string;
+}
+
 const Modal = () => {
   return (
     <div></div>
