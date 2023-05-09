1c7bb68e5000f41f3ce05143b4a1152c1f838469
Kyaw Thu <evanch98@gmail.com>
2023-05-09 13:21:36 +0800

    add a new div

--- app/components/navbar/search.tsx
+++ app/components/navbar/search.tsx
@@ -31,6 +31,20 @@ const Search = () => {
         >
           Anywhere
         </div>
+        <div
+          className="
+            hidden
+            sm:block
+            text-sm
+            font-semibold
+            px-6
+            border-x-[1px]
+            flex-1
+            text-center
+          "
+        >
+          Any Week
+        </div>
       </div>
     </div>
   );
