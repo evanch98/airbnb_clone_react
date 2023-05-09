57caa21b0965adb312747d1a82de827d47f5ed24
Kyaw Thu <evanch98@gmail.com>
2023-05-09 14:40:08 +0800

    add an interface MenuItemProps

--- app/components/navbar/MenuItem.tsx
+++ app/components/navbar/MenuItem.tsx
@@ -1,6 +1,14 @@
 'use client';
 
-const MenuItem = () => {
+interface MenuItemProps {
+  onClick: () => void;
+  label: string;
+}
+
+const MenuItem: React.FC<MenuItemProps> = ({
+  onClick,
+  label
+}) => {
   return ( 
     <div></div>
    );
