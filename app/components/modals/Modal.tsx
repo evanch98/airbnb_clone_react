95dbf3cd6dc1bd8d3d740e0c3b6bf1f1cab7b814
Kyaw Thu <evanch98@gmail.com>
2023-05-09 23:17:29 +0800

    add title

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -145,6 +145,9 @@ const Modal: React.FC<ModalProps> = ({
                 >
                   <IoMdClose size={18} />
                 </button>
+                <div className="text-lg font-semibold">
+                  {title}
+                </div>
               </div>
             </div>
           </div>
