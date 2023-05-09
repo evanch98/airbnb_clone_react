2a3064ed0683610000ec41e86dd59f3c3f234d00
Kyaw Thu <evanch98@gmail.com>
2023-05-09 23:17:15 +0800

    add Modal for testing the Modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
-          <Modal />
+          <Modal title="Hello World" isOpen />
           <Navbar />
         </ClientOnly>
         {children}
