582ce7479b26be50e5db4e17af109403732d7cd4
Kyaw Thu <evanch98@gmail.com>
2023-05-09 14:36:42 +0800

    add the MenuItem component to the UserMenu

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -3,6 +3,7 @@
 import { AiOutlineMenu } from 'react-icons/ai';
 import Avatar from '../Avatar';
 import { useCallback, useState } from 'react';
+import MenuItem from './MenuItem';
 
 const UserMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -73,6 +74,7 @@ const UserMenu = () => {
         >
           <div className='flex flex-col cursor-pointer'>
             <>
+              <MenuItem />
             </>
           </div>
         </div>
