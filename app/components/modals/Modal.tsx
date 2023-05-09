06e8f604679ab06fb16746d950f4315279ea6519
Kyaw Thu <evanch98@gmail.com>
2023-05-09 17:33:24 +0800

    style the second div

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -76,7 +76,22 @@ const Modal: React.FC<ModalProps> = ({
           focus:outline-none
           bg-neutral-800/70
         "
-      ></div>
+      >
+        <div
+          className="
+            relative
+            w-full
+            md:w-4/6
+            lg:w-3/6
+            xl:w-2/5
+            my-6
+            mx-auto
+            h-full
+            lg:h-auto
+            md:h-auto
+          "
+        ></div>
+      </div>
     </>
   );
 }
