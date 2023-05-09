068eef962b02b51ded82a49214a62e816911c358
Kyaw Thu <evanch98@gmail.com>
2023-05-09 23:44:59 +0800

    style the button

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -18,7 +18,26 @@ const Button: React.FC<ButtonProps> = ({
   icon
 }) => {
   return ( 
-    <button></button>
+    <button
+      className={`
+        relative
+        disabled:opacity-70
+        disabled:cursor-not-allowed
+        rounded-lg
+        hover:opacity-80
+        transition
+        w-full
+        ${outline ? 'bg-white' : 'bg-rose-500'}
+        ${outline ? 'border-black' : 'border-rose-500'}
+        ${outline ? 'text-black' : 'text-white'}
+        ${small ? 'py-1' : 'py-3'}
+        ${small ? 'text-sm' : 'text-md'}
+        ${small ? 'font-light' : 'font-semibold'}
+        ${small ? 'border-[1px]' : 'border-2'}
+      `}
+    >
+      {label}
+    </button>
    );
 }
  
