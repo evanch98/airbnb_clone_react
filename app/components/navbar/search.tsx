d04439be93dd13b875e3bfc648c9a585f969d8e7
Kyaw Thu <evanch98@gmail.com>
2023-05-09 13:14:18 +0800

    add className to the div

--- app/components/navbar/search.tsx
+++ app/components/navbar/search.tsx
@@ -1,6 +1,17 @@
 const Search = () => {
   return (
-    <div>
+    <div
+      className="
+        border-[1px]
+        w-full
+        md:w-auto
+        py-2
+        rounded-full
+        shadow-sm
+        hover:shadow-md
+        cursor-pointer
+      "
+    >
 
     </div>
   );
