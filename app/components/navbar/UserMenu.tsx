61d6cc86459c0ee771f9350aab6d14d1ad46fb30
Kyaw Thu <evanch98@gmail.com>
2023-05-09 13:54:51 +0800

    update the UserMenu component

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -2,7 +2,27 @@
 
 const UserMenu = () => {
   return (
-    <div></div>
+    <div className="relative">
+      <div className="flex flex-row items-center gap-3">
+        <div
+          onClick={() => {}}
+          className="
+            hidden
+            md:block
+            text-sm
+            font-semibold
+            py-3
+            px-3
+            rounded-full
+            hover:bg-neutral-100
+            transition
+            cursor-pointer
+          "
+        >
+          Airbnb your home
+        </div>
+      </div>
+    </div>
   );
 }
 
