c8997f0c608052168990e20525259ee108c0eb0f
Kyaw Thu <evanch98@gmail.com>
2023-05-10 13:29:39 +0800

    add the Heading component in the bodyContent

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -11,6 +11,7 @@ import {
 } from "react-hook-form";
 import useRegisterModal from "@/app/hooks/useRegisterModal";
 import Modal from "./Modal";
+import Heading from "../Heading";
 
 const RegisterModal = () => {
   const registerModal = useRegisterModal();
@@ -49,7 +50,9 @@ const RegisterModal = () => {
   }
 
   const bodyContent = (
-    <div className="flex flex-col gap-4"></div>
+    <div className="flex flex-col gap-4">
+      <Heading />
+    </div>
   );
 
   return (
