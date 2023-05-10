63c7dc0caed6eecaaf5256d3b0b293bf447d4063
Kyaw Thu <evanch98@gmail.com>
2023-05-10 20:42:21 +0800

    add an option to log in if the user already have an account

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -101,6 +101,38 @@ const RegisterModal = () => {
         icon={AiFillGithub}
         onClick={() => { }}
       />
+      <div
+        className="
+          text-neutral-500
+          text-center
+          mt-4
+          font-light
+        "
+      >
+        <div 
+          className="
+            justify-center 
+            flex 
+            flex-row 
+            items-center 
+            gap-2
+          "
+        >
+          <div>
+            Already have an account?
+          </div>
+          <div
+            onClick={registerModal.onClose}
+            className="
+              text-neutral-800
+              cursor-pointer
+              hover:underline
+            "
+          >
+            Log in
+          </div>
+        </div>
+      </div>
     </div>
   );
 
