635fb574b4132fd1509a1388b01bb308494baf35
Kyaw Thu <evanch98@gmail.com>
2023-05-10 12:27:29 +0800

    add a logic to open signup box

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -4,8 +4,10 @@ import { AiOutlineMenu } from 'react-icons/ai';
 import Avatar from '../Avatar';
 import { useCallback, useState } from 'react';
 import MenuItem from './MenuItem';
+import useRegisterModal from '@/app/hooks/useRegisterModal';
 
 const UserMenu = () => {
+  const registerModal = useRegisterModal();
   const [isOpen, setIsOpen] = useState(false);
 
   // reverse the current value of isOpen
@@ -80,7 +82,7 @@ const UserMenu = () => {
                 label="Login"
               />
               <MenuItem
-                onClick={() => { }}
+                onClick={registerModal.onOpen}
                 label="Sign up"
               />
             </>
