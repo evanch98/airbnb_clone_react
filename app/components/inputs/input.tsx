337296389715e45a23fecbce9fa907058c223442
Kyaw Thu <evanch98@gmail.com>
2023-05-10 19:54:10 +0800

    add an interface, InputProps

--- app/components/inputs/input.tsx
+++ app/components/inputs/input.tsx
@@ -1,5 +1,22 @@
 'use client';
 
+import {
+  FieldErrors,
+  FieldValues,
+  UseFormRegister
+} from "react-hook-form";
+
+interface InputProps {
+  id: string;
+  label: string;
+  type?: string;
+  disabled?: boolean;
+  formatPrice?: boolean;
+  required?: boolean;
+  register: UseFormRegister<FieldValues>;
+  errors: FieldErrors
+}
+
 const Input = () => {
   return ( 
     <div></div>
