a2d7028935b1dc6e38cb4c0b866fcb98ba822fa3
Kyaw Thu <evanch98@gmail.com>
2023-05-10 12:02:19 +0800

    import necessary packages

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -1,6 +1,15 @@
 'use client';
 
 import axios from "axios";
+import { AiFillGithub } from "react-icons/ai";
+import { FcGoogle } from "react-icons/fc";
+import { useCallback, useState } from "react";
+import {
+  FieldValues,
+  SubmitHandler,
+  useForm
+} from "react-hook-form";
+import useRegisterModal from "@/app/hooks/useRegisterModal";
 
 const RegisterModal = () => {
   return ( 
