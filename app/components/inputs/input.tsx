ac6c78fe2369009acccd2e440bfd1dfc93655c0b
Kyaw Thu <evanch98@gmail.com>
2023-05-10 20:12:03 +0800

    add label and the animation

--- app/components/inputs/input.tsx
+++ app/components/inputs/input.tsx
@@ -65,6 +65,25 @@ const Input: React.FC<InputProps> = ({
           ${errors[id] ? 'focus:border-rose-500' : 'focus:border-black'}
         `}
       />
+      <label
+        className={`
+          absolute
+          text-md
+          duration-150
+          transform
+          -translate-y-3
+          top-5
+          z-10
+          origin-[0]
+          ${formatPrice ? 'left-9' : 'left-4'}
+          peer-placeholder-shown:scale-100
+          peer-placeholder-shown:translate-y-0
+          peer-focus:scale-75
+          peer-focus:-translate-y-4
+        `}
+      >
+        {label}
+      </label>
     </div>
    );
 }
