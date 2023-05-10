870225c4872ebe8d8e0b80a39417615c312da554
Kyaw Thu <evanch98@gmail.com>
2023-05-10 20:32:57 +0800

    add a Button component to footerContent

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -14,6 +14,7 @@ import Modal from "./Modal";
 import Heading from "../Heading";
 import Input from "../inputs/input";
 import { toast } from "react-hot-toast";
+import Button from "../Button";
 
 const RegisterModal = () => {
   const registerModal = useRegisterModal();
@@ -86,7 +87,15 @@ const RegisterModal = () => {
   );
 
   const footerContent = (
-    <div className="flex flex-col gap-4 mt-3"></div>
+    <div className="flex flex-col gap-4 mt-3">
+      <hr />
+      <Button 
+        outline
+        label="Continue with Google"
+        icon={FcGoogle}
+        onClick={() => {}}
+      />
+    </div>
   );
 
   return (
