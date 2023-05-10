e8db747e0c1c5dfb73fdbdd1a56ad1472f05def3
Kyaw Thu <evanch98@gmail.com>
2023-05-10 20:05:48 +0800

    style the input field

--- app/components/inputs/input.tsx
+++ app/components/inputs/input.tsx
@@ -46,6 +46,24 @@ const Input: React.FC<InputProps> = ({
         disabled={disabled}
         {...register(id, { required })}
         placeholder=" "
+        type={type}
+        className={`
+          peer
+          w-full
+          p-4
+          pt-6
+          font-light
+          bg-white
+          border-2
+          rounded-md
+          outline-none
+          transition
+          disabled:opacity-70
+          disabled:cursor-not-allowed
+          ${formatPrice ? 'pl-9' : 'pl-4'}
+          ${errors[id] ? 'border-rose-500' : 'border-neutral-300'}
+          ${errors[id] ? 'focus:border-rose-500' : 'focus:border-black'}
+        `}
       />
     </div>
    );
