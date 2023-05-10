60d5baf0f05f1a6a26496b312b743bf8ce5f5bcf
Kyaw Thu <evanch98@gmail.com>
2023-05-10 13:35:32 +0800

    add new contents to Heading

--- app/components/Heading.tsx
+++ app/components/Heading.tsx
@@ -16,7 +16,9 @@ const Heading: React.FC<HeadingProps> = ({
       <div className="text-2xl font-bold">
         {title}
       </div>
-      <div></div>
+      <div className="font-light text-neutral-500 mt-2">
+        {subtitle}
+      </div>
     </div>
    );
 }
