d55f612f2a29dac9a440353abc77482e3de79563
Kyaw Thu <evanch98@gmail.com>
2023-05-12 02:35:32 +0800

    add another image domain (google)

--- next.config.js
+++ next.config.js
@@ -5,7 +5,8 @@ const nextConfig = {
   },
   images: {
     domains: [
-      "avatars.githubusercontent.com"
+      "avatars.githubusercontent.com",
+      "lh3.googleusercontent.com"
     ]
   }
 }
