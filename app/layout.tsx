c6dfe7f8565c9e12108e8c68bb1cf7f208bea070
Kyaw Thu <evanch98@gmail.com>
2023-05-11 21:28:28 +0800

    add suppressHydrationWarning={true}

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default async function RootLayout({
   const currentUser = await getCurrentUser();
   return (
     <html lang="en">
-      <body className={font.className}>
+      <body className={font.className} suppressHydrationWarning={true}>
         <ClientOnly>
           <ToasterProvider />
           <RegisterModal />
