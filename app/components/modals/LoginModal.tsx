15e50f72636551429257d87c482a5d941bd87c05
Kyaw Thu <evanch98@gmail.com>
2023-05-12 02:25:34 +0800

    add a functionality to allow user sign in using github account

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -99,7 +99,7 @@ const LoginModal = () => {
         outline
         label="Continue with GitHub"
         icon={AiFillGithub}
-        onClick={() => { }}
+        onClick={() => signIn('github')}
       />
       <div
         className="
