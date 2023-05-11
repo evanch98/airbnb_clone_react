87fa2d72994ffc72a4a18e3eb07b4b717d653676
Kyaw Thu <evanch98@gmail.com>
2023-05-11 21:14:36 +0800

    add an interface NavbarProps to add parameter to the Navbar component

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -4,8 +4,16 @@ import Container from "../Container";
 import Logo from "./Logo";
 import UserMenu from "./UserMenu";
 import Search from "./Search";
+import { User } from "@prisma/client";
 
-const Navbar = () => {
+interface NavbarProps {
+  currentUser?: User | null;
+}
+
+const Navbar: React.FC<NavbarProps> = ({
+  currentUser
+}) => {
+  console.log({currentUser});
   return (
     <div className="fixed w-full bg-white z-10 shadow-sm">
       <div
