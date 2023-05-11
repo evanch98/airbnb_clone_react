9400cf93b025d0d8b2e1a6ef4c658fae70e979b8
Kyaw Thu <evanch98@gmail.com>
2023-05-12 02:20:12 +0800

    add a logic to show user image if they added an image

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -65,7 +65,7 @@ const UserMenu: React.FC<UserMenuProps> = ({
         >
           <AiOutlineMenu />
           <div className='hidden md:block'>
-            <Avatar />
+            <Avatar src={currentUser?.image} />
           </div>
         </div>
       </div>
