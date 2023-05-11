d191e168c6be9cc3fe2db8b23def23a8c1b4f415
Kyaw Thu <evanch98@gmail.com>
2023-05-11 23:48:12 +0800

    add a logic to show the menu items appropriately depending on whether the user is logged in or not

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -85,16 +85,46 @@ const UserMenu: React.FC<UserMenuProps> = ({
           "
         >
           <div className='flex flex-col cursor-pointer'>
-            <>
-              <MenuItem 
-                onClick={loginModal.onOpen}
-                label="Login"
-              />
-              <MenuItem
-                onClick={registerModal.onOpen}
-                label="Sign up"
-              />
-            </>
+            {currentUser ? (
+              <>
+                <MenuItem
+                  onClick={() => {}}
+                  label="My trips"
+                />
+                <MenuItem
+                  onClick={() => {}}
+                  label="My favorites"
+                />
+                <MenuItem
+                  onClick={() => { }}
+                  label="My reservations"
+                />
+                <MenuItem
+                  onClick={() => { }}
+                  label="My properties"
+                />
+                <MenuItem
+                  onClick={() => { }}
+                  label="Airbnb my home"
+                />
+                <hr />
+                <MenuItem
+                  onClick={() => { }}
+                  label="Logout"
+                />
+              </>
+            ) : (
+              <>
+                <MenuItem 
+                  onClick={loginModal.onOpen}
+                  label="Login"
+                />
+                <MenuItem
+                  onClick={registerModal.onOpen}
+                  label="Sign up"
+                />
+              </>
+            )}
           </div>
         </div>
       )}
