909bcf6b0a67dbac823750e9754d9ece4422ca31
Kyaw Thu <evanch98@gmail.com>
2023-05-11 18:21:26 +0800

    add a logic to encrypt password and store the user data in the database

---
+++ app/api/register/route.ts
@@ -0,0 +1,29 @@
+import bcrypt from "bcrypt";
+
+import prisma from "@/app/libs/prismadb";
+import { NextResponse } from "next/server";
+
+export async function POST(
+  request: Request
+) {
+  const body = await request.json();
+  const {
+    email,
+    name,
+    password
+  } = body;
+
+  // encrypt the password
+  const hashedPassword = await bcrypt.hash(password, 12);
+
+  // to store the user data in the database
+  const user = await prisma.user.create({
+    data: {
+      email,
+      name,
+      hashedPassword
+    }
+  });
+
+  return NextResponse.json(user);
+}
