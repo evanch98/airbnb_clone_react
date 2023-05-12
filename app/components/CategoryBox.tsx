5a41e6b96f967fc2017c704fcdef58965c3376d1
Kyaw Thu <evanch98@gmail.com>
2023-05-12 14:07:10 +0800

    add parameters icon, label, and selected

--- app/components/CategoryBox.tsx
+++ app/components/CategoryBox.tsx
@@ -1,6 +1,18 @@
 'use client';
 
-const CategoryBox = () => {
+import { IconType } from "react-icons";
+
+interface CategoryBoxProps {
+  icon: IconType;
+  label: string;
+  selected?: boolean;
+}
+
+const CategoryBox: React.FC<CategoryBoxProps> = ({
+  icon,
+  label,
+  selected
+}) => {
   return ( 
     <div></div>
    );
