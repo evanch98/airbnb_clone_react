3f78ad543d9c270902c6246675ede974d05f707c
Kyaw Thu <evanch98@gmail.com>
2023-05-12 22:50:54 +0800

    add a logic to close the login modal and open the register modal when the user clicks the 'create an account'

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -60,6 +60,11 @@ const LoginModal = () => {
     })
   }
 
+  const toggle = useCallback(() => {
+    loginModal.onClose();
+    registerModal.onOpen();
+  }, [loginModal, registerModal]);
+
   const bodyContent = (
     <div className="flex flex-col gap-4">
       <Heading
@@ -119,17 +124,17 @@ const LoginModal = () => {
           "
         >
           <div>
-            Already have an account?
+            First time using Airbnb?
           </div>
           <div
-            onClick={registerModal.onClose}
+            onClick={toggle}
             className="
               text-neutral-800
               cursor-pointer
               hover:underline
             "
           >
-            Log in
+            Create an account
           </div>
         </div>
       </div>
