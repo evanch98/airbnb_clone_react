c37527c3f50e3ce2abce66b05904397da3e59840
Kyaw Thu <evanch98@gmail.com>
2023-05-12 13:58:16 +0800

    add some categories in the list

--- app/components/navbar/Categories.tsx
+++ app/components/navbar/Categories.tsx
@@ -1,6 +1,28 @@
 'use client';
 
+import { TbBeach } from "react-icons/tb";
+import { GiWindmill } from "react-icons/gi";
+import { MdOutlineVilla } from "react-icons/md";
+
 import Container from "../Container";
+
+export const categories = [
+  {
+    label: "Beach",
+    icon: TbBeach,
+    description: "This property is close to the beach!"
+  },
+  {
+    label: "Windmills",
+    icon: GiWindmill,
+    description: "This property has windmills!"
+  },
+  {
+    label: "Modern",
+    icon: MdOutlineVilla,
+    description: "This property is modern!"
+  },
+]
 
 const Categories = () => {
   return ( 
