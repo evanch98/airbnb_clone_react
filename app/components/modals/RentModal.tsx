39c6b968def840295516987734a862ad495af450
Kyaw Thu <evanch98@gmail.com>
2023-05-13 22:46:57 +0800

    add enum STEPS

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -3,6 +3,15 @@
 import useRentModal from "@/app/hooks/useRentModal";
 import Modal from "./Modal";
 
+enum STEPS {
+  CATEGORY = 0,
+  LOCATION = 1,
+  INFO = 2,
+  IMAGES = 3,
+  DESCRIPTION = 4,
+  PRICE = 5
+}
+
 const RentModal = () => {
   const rentModal = useRentModal();
 
