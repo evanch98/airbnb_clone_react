1ec80bc7107742bcbacef33ae77c77605dd9759d
Kyaw Thu <evanch98@gmail.com>
2023-05-13 22:40:25 +0800

    add the RentModal component

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import RegisterModal from './components/modals/RegisterModal'
 import ToasterProvider from './providers/ToasterProvider'
 import LoginModal from './components/modals/LoginModal'
 import getCurrentUser from './actions/getCurrentUser'
+import RentModal from './components/modals/RentModal'
 
 const font = Nunito({ subsets: ['latin'] })
 
@@ -27,6 +28,7 @@ export default async function RootLayout({
           <ToasterProvider />
           <RegisterModal />
           <LoginModal />
+          <RentModal />
           <Navbar currentUser={currentUser} />
         </ClientOnly>
         {children}
