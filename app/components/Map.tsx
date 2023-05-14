5439c9e530dc0c6f64e120f84725c93989407c4a
Kyaw Thu <evanch98@gmail.com>
2023-05-15 01:50:32 +0800

    configuration for the leaflet

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -9,6 +9,16 @@ import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
 import markerIcon from "leaflet/dist/images/marker-icon.png";
 import markerShadow from "leaflet/dist/images/marker-shadow.png";
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+// configuration to be able to use leaflet since it is not fully supported by React
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+	iconUrl: markerIcon.src,
+	iconRetinaUrl: markerIcon2x.src,
+	shadowUrl: markerShadow.src
+});
+
 const Map = () => {
 	return ( 
 		<div></div>
