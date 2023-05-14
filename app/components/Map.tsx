7ddbbafef226af3cd4ec14464c69989da261296d
Kyaw Thu <evanch98@gmail.com>
2023-05-15 02:17:11 +0800

    add marker on the map

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -37,6 +37,11 @@ const Map: React.FC<MapProps> = ({
 				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 			/>
+			{center && (
+				<Marker 
+					position={center as L.LatLngExpression}
+				/>
+			)}
 		</MapContainer>
 	);
 };
