85c121293eb8ee4809f4c47341830f878970157d
Kyaw Thu <evanch98@gmail.com>
2023-05-15 01:47:03 +0800

    add required imports

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -1,6 +1,13 @@
 "use client";
 
 import React from "react";
+import L from "leaflet";
+import { MapContainer, Marker, TileLayer } from "react-leaflet";
+
+import "leaflet/dist/leaflet.css";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
 
 const Map = () => {
 	return ( 
