c9c8b81928ce10413651e5bc9316c99501242490
Kyaw Thu <evanch98@gmail.com>
2023-05-14 22:49:23 +0800

    add contents in the CategoryInput component

--- app/components/inputs/CategoryInput.tsx
+++ app/components/inputs/CategoryInput.tsx
@@ -16,7 +16,26 @@ const CategoryInput: React.FC<CategoryInputProps> = ({
   onClick
 }) => {
   return ( 
-    <div></div>
+    <div
+      onClick={() => onClick(label)}
+      className={`
+        rounded-xl
+        border-2
+        p-4
+        flex
+        flex-col
+        gap-3
+        hover:border-black
+        transition
+        cursor-pointer
+        ${selected ? 'border-black' : 'border-neutral-200'}
+      `}
+    >
+      <Icon size={30} />
+      <div className="font-semibold">
+        {label}
+      </div>
+    </div>
   );
 }
 
