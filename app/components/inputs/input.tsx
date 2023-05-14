7248f0ccd972af9a05cebc4e11b487134817d179
Kyaw Thu <evanch98@gmail.com>
2023-05-15 01:29:38 +0800

    reformat the document

--- app/components/inputs/input.tsx
+++ app/components/inputs/input.tsx
@@ -1,9 +1,10 @@
-'use client';
+"use client";
 
+import React from "react";
 import {
-  FieldErrors,
-  FieldValues,
-  UseFormRegister
+	FieldErrors,
+	FieldValues,
+	UseFormRegister
 } from "react-hook-form";
 import { BiDollar } from "react-icons/bi";
 
@@ -19,35 +20,35 @@ interface InputProps {
 }
 
 const Input: React.FC<InputProps> = ({
-  id,
-  label,
-  type = "text",
-  disabled,
-  formatPrice,
-  required,
-  register,
-  errors
+	id,
+	label,
+	type = "text",
+	disabled,
+	formatPrice,
+	required,
+	register,
+	errors
 }) => {
-  return ( 
-    <div className="w-full relative">
-      {formatPrice && (
-        <BiDollar 
-          size={24}
-          className="
+	return ( 
+		<div className="w-full relative">
+			{formatPrice && (
+				<BiDollar 
+					size={24}
+					className="
             text-neutral-700
             absolute
             top-5
             left-2
           "
-        />
-      )}
-      <input 
-        id={id}
-        disabled={disabled}
-        {...register(id, { required })}
-        placeholder=" "
-        type={type}
-        className={`
+				/>
+			)}
+			<input 
+				id={id}
+				disabled={disabled}
+				{...register(id, { required })}
+				placeholder=" "
+				type={type}
+				className={`
           peer
           w-full
           p-4
@@ -60,13 +61,13 @@ const Input: React.FC<InputProps> = ({
           transition
           disabled:opacity-70
           disabled:cursor-not-allowed
-          ${formatPrice ? 'pl-9' : 'pl-4'}
-          ${errors[id] ? 'border-rose-500' : 'border-neutral-300'}
-          ${errors[id] ? 'focus:border-rose-500' : 'focus:border-black'}
+          ${formatPrice ? "pl-9" : "pl-4"}
+          ${errors[id] ? "border-rose-500" : "border-neutral-300"}
+          ${errors[id] ? "focus:border-rose-500" : "focus:border-black"}
         `}
-      />
-      <label
-        className={`
+			/>
+			<label
+				className={`
           absolute
           text-md
           duration-150
@@ -75,18 +76,18 @@ const Input: React.FC<InputProps> = ({
           top-5
           z-10
           origin-[0]
-          ${formatPrice ? 'left-9' : 'left-4'}
+          ${formatPrice ? "left-9" : "left-4"}
           peer-placeholder-shown:scale-100
           peer-placeholder-shown:translate-y-0
           peer-focus:scale-75
           peer-focus:-translate-y-4
-          ${errors[id] ? 'text-rose-500' : 'text-zinc-400'}
+          ${errors[id] ? "text-rose-500" : "text-zinc-400"}
         `}
-      >
-        {label}
-      </label>
-    </div>
-   );
+			>
+				{label}
+			</label>
+		</div>
+	);
 }
  
 export default Input;
