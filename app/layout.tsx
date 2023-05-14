2b4c6ac0a0577fc55e98467261cf614278b57cf2
Kyaw Thu <evanch98@gmail.com>
2023-05-15 01:27:17 +0800

    reformat the document

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,38 +1,39 @@
-import './globals.css'
-import { Nunito } from 'next/font/google'
-import Navbar from './components/navbar/Navbar'
-import ClientOnly from './components/navbar/ClientOnly'
-import RegisterModal from './components/modals/RegisterModal'
-import ToasterProvider from './providers/ToasterProvider'
-import LoginModal from './components/modals/LoginModal'
-import getCurrentUser from './actions/getCurrentUser'
-import RentModal from './components/modals/RentModal'
+import React from "react";
+import "./globals.css";
+import { Nunito } from "next/font/google";
+import Navbar from "./components/navbar/Navbar";
+import ClientOnly from "./components/navbar/ClientOnly";
+import RegisterModal from "./components/modals/RegisterModal";
+import ToasterProvider from "./providers/ToasterProvider";
+import LoginModal from "./components/modals/LoginModal";
+import getCurrentUser from "./actions/getCurrentUser";
+import RentModal from "./components/modals/RentModal";
 
-const font = Nunito({ subsets: ['latin'] })
+const font = Nunito({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Airbnb',
-  description: 'Airbnb clone',
-}
+	title: "Airbnb",
+	description: "Airbnb clone",
+};
 
 export default async function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
-  const currentUser = await getCurrentUser();
-  return (
-    <html lang="en">
-      <body className={font.className} suppressHydrationWarning={true}>
-        <ClientOnly>
-          <ToasterProvider />
-          <RegisterModal />
-          <LoginModal />
-          <RentModal />
-          <Navbar currentUser={currentUser} />
-        </ClientOnly>
-        {children}
-      </body>
-    </html>
-  )
+	const currentUser = await getCurrentUser();
+	return (
+		<html lang="en">
+			<body className={font.className} suppressHydrationWarning={true}>
+				<ClientOnly>
+					<ToasterProvider />
+					<RegisterModal />
+					<LoginModal />
+					<RentModal />
+					<Navbar currentUser={currentUser} />
+				</ClientOnly>
+				{children}
+			</body>
+		</html>
+	);
 }
