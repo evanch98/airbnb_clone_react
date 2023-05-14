fe5e21cdb196e962a00d9b7916fa13643f0e461a
Kyaw Thu <evanch98@gmail.com>
2023-05-15 01:02:43 +0800

    add the Map component

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -8,6 +8,7 @@ import { categories } from "../navbar/Categories";
 import CategoryInput from "../inputs/CategoryInput";
 import { FieldValues, useForm } from "react-hook-form";
 import CountrySelect from "../inputs/CountrySelect";
+import Map from "../Map";
 
 // steps of renting a home
 enum STEPS {
@@ -137,6 +138,7 @@ const RentModal = () => {
           value={location}
           onChange={(value) => setCustomValue("location", value)}
         />
+        <Map />
       </div>
     );
   }
