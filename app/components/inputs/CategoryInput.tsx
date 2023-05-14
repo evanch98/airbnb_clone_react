c2bf678834bf953dee1af032c370becdea111dd8
Kyaw Thu <evanch98@gmail.com>
2023-05-14 22:31:49 +0800

    add new parameters

--- app/components/inputs/CategoryInput.tsx
+++ app/components/inputs/CategoryInput.tsx
@@ -1,9 +1,23 @@
 'use client';
 
-const CategoryInput = () => {
+import { IconType } from "react-icons";
+
+interface CategoryInputProps {
+  icon: IconType;
+  label: string;
+  selected: boolean;
+  onClick: (value: string) => void;
+}
+
+const CategoryInput: React.FC<CategoryInputProps> = ({
+  icon,
+  label,
+  selected,
+  onClick
+}) => {
   return ( 
     <div></div>
   );
 }
- 
+
 export default CategoryInput;
