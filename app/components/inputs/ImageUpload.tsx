31b7c9e05daabd5ba1066f65ec9e55637679591a
Kyaw Thu <evanch98@gmail.com>
2023-05-15 20:09:35 +0800

    add necessary imports and declare a global variable cloudinary of type any

--- app/components/inputs/ImageUpload.tsx
+++ app/components/inputs/ImageUpload.tsx
@@ -1,6 +1,14 @@
 "use client";
 
-import React from "react";
+import React, { useCallback } from "react";
+import { CldUploadWidget } from "next-cloudinary";
+import Image from "next/image";
+import { TbPhotoPlus } from "react-icons/tb";
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  let cloudinary: any
+}
 
 const ImageUpload = () => {
 	return ( 
