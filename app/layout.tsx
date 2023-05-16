8b0977564dbae97e6db34ca886128d1de2bea01c
Kyaw Thu <evanch98@gmail.com>
2023-05-17 00:40:52 +0800

    wrap the 'children' inside the div

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export default async function RootLayout({
 					<RentModal />
 					<Navbar currentUser={currentUser} />
 				</ClientOnly>
-				{children}
+				<div className="pb-20 pt-28">{children}</div>
 			</body>
 		</html>
 	);
