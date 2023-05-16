349bea530d9c6d27b2913cc48f0f53ef640e852d
Kyaw Thu <evanch98@gmail.com>
2023-05-16 12:44:06 +0800

    add a button to let user upload an image

--- app/components/inputs/ImageUpload.tsx
+++ app/components/inputs/ImageUpload.tsx
@@ -33,7 +33,27 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
 		>
 			{({ open }) => {
 				return (
-					<div></div>
+					<div
+						onClick={() => open?.()}
+						className="
+							relative
+							cursor-pointer
+							hover:opacity-70
+							transition
+							border-dashed
+							border-2
+							p-20
+							border-neutral-300
+							flex
+							flex-col
+							justify-center
+							items-center
+							gap-4
+							text-neutral-600
+						"
+					>
+						<TbPhotoPlus />
+					</div>
 				);
 			}}
 		</CldUploadWidget>
