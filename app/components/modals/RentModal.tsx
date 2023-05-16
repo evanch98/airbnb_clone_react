55a0b0ce8909744b3042decbda4b50cc85b0ad62
Kyaw Thu <evanch98@gmail.com>
2023-05-16 20:59:17 +0800

    change the onSubmit value from onNext to handleSubmit(onSubmit)

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -301,7 +301,7 @@ const RentModal = () => {
 		<Modal
 			isOpen={rentModal.isOpen}
 			onClose={rentModal.onClose}
-			onSubmit={onNext}
+			onSubmit={handleSubmit(onSubmit)}
 			actionLabel={actionLabel}
 			secondaryActionLabel={secondaryActionLabel}
 			secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
