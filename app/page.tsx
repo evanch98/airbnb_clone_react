1bbca5e5f523825b3ce8eae358196bc69b485a3a
Kyaw Thu <evanch98@gmail.com>
2023-05-17 00:46:50 +0800

    add contents to the Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,27 @@
-import React from "react"
+import React from "react";
+import ClientOnly from "./components/navbar/ClientOnly";
+import Container from "./components/Container";
 
 export default function Home() {
 	return (
-		<div>Hello Airbnb!</div>
-	)
+		<ClientOnly>
+			<Container>
+				<div
+					className="
+          pt-24
+          grid
+          grid-cols-1
+          sm:grid-cols-2
+          md:grid-cols-3
+          lg:grid-cols-4
+          xl:grid-cols-5
+          2xl:grid-cols-6
+          gap-8
+        "
+				>
+					<div>My future listings</div>
+				</div>
+			</Container>
+		</ClientOnly>
+	);
 }
