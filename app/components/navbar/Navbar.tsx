ba01b4ae791ae31a459c43412fa3ab1585f8e061
Kyaw Thu <evanch98@gmail.com>
2023-05-17 12:08:53 +0800

    reformat the code

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,5 +1,6 @@
-'use client';
+"use client";
 
+import React from "react";
 import Container from "../Container";
 import Logo from "./Logo";
 import UserMenu from "./UserMenu";
@@ -8,23 +9,21 @@ import { SafeUser } from "@/app/types";
 import Categories from "./Categories";
 
 interface NavbarProps {
-  currentUser?: SafeUser | null;
+	currentUser?: SafeUser | null;
 }
 
-const Navbar: React.FC<NavbarProps> = ({
-  currentUser
-}) => {
-  return (
-    <div className="fixed w-full bg-white z-10 shadow-sm">
-      <div
-        className="
+const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
+	return (
+		<div className="fixed w-full bg-white z-10 shadow-sm">
+			<div
+				className="
           py-4
           border-b-[1px]
         "
-      >
-        <Container>
-          <div
-            className="
+			>
+				<Container>
+					<div
+						className="
               flex
               flex-row
               items-center
@@ -32,16 +31,16 @@ const Navbar: React.FC<NavbarProps> = ({
               gap-3
               md:gap-0
             "
-          >
-            <Logo />
-            <Search />
-            <UserMenu currentUser={currentUser} />
-          </div>
-        </Container>
-      </div>
-      <Categories />
-    </div>
-  );
-}
+					>
+						<Logo />
+						<Search />
+						<UserMenu currentUser={currentUser} />
+					</div>
+				</Container>
+			</div>
+			<Categories />
+		</div>
+	);
+};
 
 export default Navbar;
