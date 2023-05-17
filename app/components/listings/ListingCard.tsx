a9cc14e2fef21fc69d7c31d137fe585b4929785a
Kyaw Thu <evanch98@gmail.com>
2023-05-17 12:33:47 +0800

    add new parameters

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -1,8 +1,28 @@
 "use client";
 
+import { SafeUser } from "@/app/types";
+import { Listing, Reservation } from "@prisma/client";
 import React from "react";
 
-const ListingCard = () => {
+interface ListingCardProps {
+  data: Listing;
+  reservation?: Reservation;
+  onAction?: (id: string) => void;
+  disable?: boolean;
+  actionLabel?: string;
+  actionId?: string;
+  currentUser?: SafeUser | null;
+}
+
+const ListingCard: React.FC<ListingCardProps> = ({
+  data,
+  reservation,
+  onAction,
+  disable,
+  actionLabel,
+  actionId,
+  currentUser,
+}) => {
 	return ( 
 		<div>Listing Card</div>
 	);
