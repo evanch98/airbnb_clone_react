42923d9aeb210d8fbd4a64f9e731fefd0d1357d9
Kyaw Thu <evanch98@gmail.com>
2023-05-17 16:10:48 +0800

    add a new function DELETE to let use remove the listing from Favorites

--- app/api/favorites/[listingId]/route.ts
+++ app/api/favorites/[listingId]/route.ts
@@ -6,7 +6,10 @@ interface IParams {
 	listingId?: string;
 }
 
-export async function POST(request: Request, { params }: { params: IParams }) {
+export async function POST(
+	request: Request, 
+	{ params }: { params: IParams }
+) {
 	const currentUser = await getCurrentUser();
 
 	if (!currentUser) {
@@ -24,6 +27,38 @@ export async function POST(request: Request, { params }: { params: IParams }) {
 	favoriteIds.push(listingId);
 
 	const user = prisma.user.update({
+		where: {
+			id: currentUser.id,
+		},
+		data: {
+			favoriteIds,
+		},
+	});
+
+	return NextResponse.json(user);
+}
+
+export async function DELETE(
+	request: Request,
+	{ params }: { params: IParams }
+) {
+	const currentUser = await getCurrentUser();
+
+	if (!currentUser) {
+		return NextResponse.error();
+	}
+
+	const { listingId } = params;
+
+	if (!listingId || typeof listingId !== "string") {
+		throw new Error("Invalid ID");
+	}
+
+	let favoriteIds = [...(currentUser.favoriteIds || [])];
+
+	favoriteIds = favoriteIds.filter((id) => id !== listingId);
+
+	const user = await prisma.user.update({
 		where: {
 			id: currentUser.id,
 		},
