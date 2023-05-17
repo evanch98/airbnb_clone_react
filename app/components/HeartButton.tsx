ff8f2a1296f8e12cf84c3bc69140bcbbb1d4c6c8
Kyaw Thu <evanch98@gmail.com>
2023-05-17 21:24:46 +0800

    replace the hard coded hasFavorited and toggleFavorite with the hasFavorited and toggleFavorite from the useFavorite hook

--- app/components/HeartButton.tsx
+++ app/components/HeartButton.tsx
@@ -3,6 +3,7 @@
 import React from "react";
 import { SafeUser } from "../types";
 import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
+import useFavorite from "../hooks/useFavorite";
 
 interface HeartButtonProps {
 	listingId: string;
@@ -13,8 +14,7 @@ const HeartButton: React.FC<HeartButtonProps> = ({
 	listingId,
 	currentUser,
 }) => {
-	const hasFavorited = false;
-	const toggleFavorite = () => {};
+	const {hasFavorited, toggleFavorite} = useFavorite({listingId, currentUser});
 	return (
 		<div
 			onClick={toggleFavorite}
