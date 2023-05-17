3afdae45c7bd4a4d429f2730f014d8262686731f
Kyaw Thu <evanch98@gmail.com>
2023-05-17 12:08:23 +0800

    reformat the code

--- app/components/navbar/MenuItem.tsx
+++ app/components/navbar/MenuItem.tsx
@@ -1,28 +1,27 @@
-'use client';
+"use client";
+
+import React from "react";
 
 interface MenuItemProps {
-  onClick: () => void;
-  label: string;
+	onClick: () => void;
+	label: string;
 }
 
-const MenuItem: React.FC<MenuItemProps> = ({
-  onClick,
-  label
-}) => {
-  return ( 
-    <div
-      onClick={onClick}
-      className="
+const MenuItem: React.FC<MenuItemProps> = ({ onClick, label }) => {
+	return (
+		<div
+			onClick={onClick}
+			className="
         px-4
         py-3
         hover:bg-neutral-100
         transition
         font-semibold
       "
-    >
-      {label}
-    </div>
-   );
-}
- 
+		>
+			{label}
+		</div>
+	);
+};
+
 export default MenuItem;
