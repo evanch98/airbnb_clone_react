413ae85f96ad9d65695e0837524cdeb9542bfdf3
Kyaw Thu <evanch98@gmail.com>
2023-05-17 15:14:04 +0800

    add two divs to show the price

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -112,6 +112,11 @@ const ListingCard: React.FC<ListingCardProps> = ({
 				<div className="font-light text-neutral-500">
 					{reservationDate || data.category}
 				</div>
+				<div className="flex flex-row items-center gap-1">
+					<div className="font-semibold">
+            $ {price}
+					</div>
+				</div>
 			</div>
 		</div>
 	);
