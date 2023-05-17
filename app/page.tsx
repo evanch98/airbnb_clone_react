e684e14136a59c9193937dde8949b2cde3e20da8
Kyaw Thu <evanch98@gmail.com>
2023-05-17 12:28:56 +0800

    fetch the currentUser

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,11 @@ import Container from "./components/Container";
 import EmptyState from "./components/EmptyState";
 import getListings from "./actions/getListings";
 import ListingCard from "./components/listings/ListingCard";
+import getCurrentUser from "./actions/getCurrentUser";
 
 export default async function Home() {
 	const listings = await getListings();
+	const currentUser = await getCurrentUser();
 
 	if (listings.length === 0) {
 		return (
@@ -35,6 +37,7 @@ export default async function Home() {
 					{listings.map((listing: any) => {
 						return (
 							<ListingCard 
+								currentUser={currentUser}
 								key={listing.id}
 								data={listing}
 							/>
