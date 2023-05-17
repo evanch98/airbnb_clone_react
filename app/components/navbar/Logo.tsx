7f2a30929a8f8ef9689615257623c2449af679d7
Kyaw Thu <evanch98@gmail.com>
2023-05-17 12:07:43 +0800

    reformat the code

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -1,21 +1,22 @@
-'use client';
+"use client";
 
+import React from "react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 const Logo = () => {
-  const router = useRouter();
+	const router = useRouter();
 
-  return (
-    <Image
-      onClick={() => router.push("/")}
-      alt="Logo"
-      className="hidden md:block cursor-pointer"
-      height="100"
-      width="100"
-      src="/images/logo.png"
-    />
-  );
+	return (
+		<Image
+			onClick={() => router.push("/")}
+			alt="Logo"
+			className="hidden md:block cursor-pointer"
+			height="100"
+			width="100"
+			src="/images/logo.png"
+		/>
+	);
 }
 
 export default Logo;
