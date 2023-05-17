983258e5879f0d82d0d9c557e91fb39a09d1657c
Kyaw Thu <evanch98@gmail.com>
2023-05-17 12:07:11 +0800

    reformat the code

--- app/components/navbar/ClientOnly.tsx
+++ app/components/navbar/ClientOnly.tsx
@@ -1,28 +1,22 @@
-'use client';
+"use client";
 
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 interface ClientOnlyProps {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }
 
-const ClientOnly: React.FC<ClientOnlyProps> = ({
-  children
-}) => {
-  const [hasMounted, setHasMounted] = useState(false);
+const ClientOnly: React.FC<ClientOnlyProps> = ({ children }) => {
+	const [hasMounted, setHasMounted] = useState(false);
 
-  useEffect(() => {
-    setHasMounted(true);
-  }, []);
+	useEffect(() => {
+		setHasMounted(true);
+	}, []);
 
-  if (!hasMounted) {
-    return null;
-  }
-  return ( 
-    <>
-      {children}
-    </>
-   );
-}
- 
+	if (!hasMounted) {
+		return null;
+	}
+	return <>{children}</>;
+};
+
 export default ClientOnly;
