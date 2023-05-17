3069e62bd7099ecbd5b019957b9a0f8f43baaa07
Kyaw Thu <evanch98@gmail.com>
2023-05-17 16:03:05 +0800

    add a new function POST to let user add a listing to Favorites

--- app/api/favorites/[listingId]/route.ts
+++ app/api/favorites/[listingId]/route.ts
@@ -3,5 +3,34 @@ import getCurrentUser from "@/app/actions/getCurrentUser";
 import prisma from "@/app/libs/prismadb";
 
 interface IParams {
-  listingId?: string;
+	listingId?: string;
+}
+
+export async function POST(request: Request, { params }: { params: IParams }) {
+	const currentUser = await getCurrentUser();
+
+	if (!currentUser) {
+		return NextResponse.error();
+	}
+
+	const { listingId } = params;
+
+	if (!listingId || typeof listingId !== "string") {
+		throw new Error("Invalid ID");
+	}
+
+	const favoriteIds = [...(currentUser.favoriteIds || [])];
+
+	favoriteIds.push(listingId);
+
+	const user = prisma.user.update({
+		where: {
+			id: currentUser.id,
+		},
+		data: {
+			favoriteIds,
+		},
+	});
+
+	return NextResponse.json(user);
 }
