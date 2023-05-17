9b411ef24892626d8f39fa7cc07b23ed1df14b0c
Kyaw Thu <evanch98@gmail.com>
2023-05-17 13:02:15 +0800

    add contents to the ListingCard

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -4,6 +4,7 @@ import useCountries from "@/app/hooks/useCountry";
 import { SafeUser } from "@/app/types";
 import { Listing, Reservation } from "@prisma/client";
 import { format } from "date-fns";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 import React, { useCallback, useMemo } from "react";
 
@@ -66,7 +67,47 @@ const ListingCard: React.FC<ListingCardProps> = ({
 		return `${format(start, "PP")} - ${format(end, "PP")}`
 	}, [reservation]);
 
-	return <div>Listing Card</div>;
+	return (
+		<div
+			onClick={() => router.push(`/listings/${data.id}`)}
+			className="
+        col-span-1
+        cursor-pointer
+        group
+      "
+		>
+			<div className="flex flex-col gap-2 w-full">
+				<div
+					className="
+            aspect-square
+            w-full
+            relative
+            overflow-hidden
+            rounded-xl
+          "
+				>
+					<Image 
+						fill
+						alt="Listing"
+						src={data.imageSrc}
+						className="
+              object-cover
+              h-full
+              w-full
+              group-hover:scale-110
+              transition
+            "
+					/>
+					<div className="absolute top-3 right-3">
+						<HeartButton 
+							listingId={data.id}
+							currentUser={currentUser}
+						/>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default ListingCard;
