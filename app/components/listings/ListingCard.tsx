d6342d58836638d89252a7d2237b405db58e799b
Kyaw Thu <evanch98@gmail.com>
2023-05-17 12:46:11 +0800

    add a new function handleCancel

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -4,39 +4,49 @@ import useCountries from "@/app/hooks/useCountry";
 import { SafeUser } from "@/app/types";
 import { Listing, Reservation } from "@prisma/client";
 import { useRouter } from "next/navigation";
-import React from "react";
+import React, { useCallback } from "react";
 
 interface ListingCardProps {
-  data: Listing;
-  reservation?: Reservation;
-  onAction?: (id: string) => void;
-  disable?: boolean;
-  actionLabel?: string;
-  actionId?: string;
-  currentUser?: SafeUser | null;
+	data: Listing;
+	reservation?: Reservation;
+	onAction?: (id: string) => void;
+	disabled?: boolean;
+	actionLabel?: string;
+	actionId?: string;
+	currentUser?: SafeUser | null;
 }
 
 const ListingCard: React.FC<ListingCardProps> = ({
 	data,
 	reservation,
 	onAction,
-	disable,
+	disabled,
 	actionLabel,
-	actionId,
+	actionId = "",
 	currentUser,
 }) => {
 	const router = useRouter();
 	const { getByValue } = useCountries();
 
-	// the location is stored as a value in the Listing object; therefore, 
-	// retrieve the location by using the getByValue from the  useCountries 
+	// the location is stored as a value in the Listing object; therefore,
+	// retrieve the location by using the getByValue from the  useCountries
 	// hook
 	const location = getByValue(data.locationValue);
 
+	const handleCancel = useCallback(
+		(e: React.MouseEvent<HTMLButtonElement>) => {
+			e.stopPropagation();
 
-	return ( 
-		<div>Listing Card</div>
+			if (disabled) {
+				return;
+			}
+
+			onAction?.(actionId);
+		},
+		[disabled, onAction, actionId]
 	);
-}
+
+	return <div>Listing Card</div>;
+};
 
 export default ListingCard;
