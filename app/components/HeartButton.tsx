eb97514cea209fb2ef820eaefca3eb6c844f4b5c
Kyaw Thu <evanch98@gmail.com>
2023-05-17 14:58:14 +0800

    add new parameters

--- app/components/HeartButton.tsx
+++ app/components/HeartButton.tsx
@@ -1,8 +1,17 @@
 "use client";
 
 import React from "react";
+import { SafeUser } from "../types";
 
-const HeartButton = () => {
+interface HeartButtonProps {
+  listingId: string;
+  currentUse?: SafeUser | null;
+}
+
+const HeartButton: React.FC<HeartButtonProps> = ({
+	listingId,
+	currentUse,
+}) => {
 	return ( 
 		<div></div>
 	);
