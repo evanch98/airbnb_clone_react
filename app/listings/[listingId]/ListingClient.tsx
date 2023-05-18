eac0759e39cb1f7a24730b8740eb5ffdf4890428
Kyaw Thu <evanch98@gmail.com>
2023-05-18 20:32:42 +0800

    import the ListingInfo

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -2,6 +2,7 @@
 
 import Container from "@/app/components/Container";
 import ListingHead from "@/app/components/listings/ListingHead";
+import ListingInfo from "@/app/components/listings/ListingInfo";
 import { categories } from "@/app/components/navbar/Categories";
 import { SafeListing, SafeUser } from "@/app/types";
 import { Reservation } from "@prisma/client";
