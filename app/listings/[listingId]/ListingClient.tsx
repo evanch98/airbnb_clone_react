73f1ed97c363ee5a208f688190cfbcd1a22e653b
Kyaw Thu <evanch98@gmail.com>
2023-05-18 19:35:16 +0800

    add an interface ListingClientProps

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -1,6 +1,19 @@
+import { SafeListing, SafeUser } from "@/app/types";
+import { Reservation } from "@prisma/client";
 import React from "react";
 
-const ListingClient = () => {
+interface ListingClientProps {
+  reservation?: Reservation[];
+  listing: SafeListing & {
+    user: SafeUser
+  };
+  currentUser?: SafeUser | null;
+}
+
+const ListingClient: React.FC<ListingClientProps> = ({
+	listing,
+	currentUser,
+}) => {
 	return ( 
 		<div>Listing Client</div>
 	);
