73c7fe1e0e637f36abea6e5c0e6a729edcc2d7e5
Kyaw Thu <evanch98@gmail.com>
2023-05-18 20:31:08 +0800

    add new div and ListingInfo

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -34,6 +34,25 @@ const ListingClient: React.FC<ListingClientProps> = ({
 						id={listing.id}
 						currentUser={currentUser}
 					/>
+					<div
+						className="
+              grid
+              grid-cols-1
+              md:grid-cols-7
+              md:gap-10
+              mt-6
+            "
+					>
+						<ListingInfo 
+							user={listing.user}
+							category={category}
+							description={listing.description}
+							roomCount={listing.roomCount}
+							guestCount={listing.guestCount}
+							bathroomCount={listing.bathroomCount}
+							locationValue={listing.locationValue}
+						/>
+					</div>
 				</div>
 			</div>
 		</Container>
