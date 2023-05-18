778fd9d5c07441a359b88c5c6f0aba67ed4c79c8
Kyaw Thu <evanch98@gmail.com>
2023-05-18 13:22:06 +0800

    reformat the code

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -8,63 +8,63 @@ import bcrypt from "bcrypt";
 import prisma from "@/app/libs/prismadb";
 
 export const authOptions: AuthOptions = {
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID as string,
-      clientSecret: process.env.GITHUB_SECRET as string,
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-    CredentialsProvider({
-      name: 'credentials',
-      credentials: {
-        email: {label: 'email', type: "text"},
-        password: {label: 'password', type: "password"}
-      },
-      async authorize(credentials) {
-        if (!credentials?.email || !credentials?.password) {
-          throw new Error('Invalid credentials');
-        }
+	adapter: PrismaAdapter(prisma),
+	providers: [
+		GithubProvider({
+			clientId: process.env.GITHUB_ID as string,
+			clientSecret: process.env.GITHUB_SECRET as string,
+		}),
+		GoogleProvider({
+			clientId: process.env.GOOGLE_CLIENT_ID as string,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+		}),
+		CredentialsProvider({
+			name: "credentials",
+			credentials: {
+				email: {label: "email", type: "text"},
+				password: {label: "password", type: "password"}
+			},
+			async authorize(credentials) {
+				if (!credentials?.email || !credentials?.password) {
+					throw new Error("Invalid credentials");
+				}
 
-        const user = await prisma.user.findUnique({
-          where: {
-            email: credentials.email
-          }
-        });
+				const user = await prisma.user.findUnique({
+					where: {
+						email: credentials.email
+					}
+				});
 
-        // if the user does not have a hashedPassword, then it is logging in a wrong way
-        // if the user does not exist
-        // throw error
-        if (!user || !user?.hashedPassword) {
-          throw new Error('Invalid credentials');
-        }
+				// if the user does not have a hashedPassword, then it is logging in a wrong way
+				// if the user does not exist
+				// throw error
+				if (!user || !user?.hashedPassword) {
+					throw new Error("Invalid credentials");
+				}
 
-        // to check if the password entered by the user is equal to the actual password
-        const isCorrectPassword = await bcrypt.compare(
-          credentials.password,
-          user.hashedPassword
-        );
+				// to check if the password entered by the user is equal to the actual password
+				const isCorrectPassword = await bcrypt.compare(
+					credentials.password,
+					user.hashedPassword
+				);
 
-        if (!isCorrectPassword) {
-          throw new Error('Invalid credentials');
-        }
+				if (!isCorrectPassword) {
+					throw new Error("Invalid credentials");
+				}
 
-        // if there is no error, return the user
-        return user;
-      }
-    })
-  ],
-  pages: {
-    signIn: '/',
-  },
-  debug: process.env.NODE_ENV === 'development', // only enable debug in development phase
-  session: {
-    strategy: "jwt"
-  },
-  secret: process.env.NEXTAUTH_SECRET,
+				// if there is no error, return the user
+				return user;
+			}
+		})
+	],
+	pages: {
+		signIn: "/",
+	},
+	debug: process.env.NODE_ENV === "development", // only enable debug in development phase
+	session: {
+		strategy: "jwt"
+	},
+	secret: process.env.NEXTAUTH_SECRET,
 }
 
 export default NextAuth(authOptions);
