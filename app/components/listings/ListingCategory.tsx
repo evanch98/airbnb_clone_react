69c58d8023f15dd004cc095cea246fb719bc457c
Kyaw Thu <evanch98@gmail.com>
2023-05-18 21:19:15 +0800

    add contents to the ListingCategory

--- app/components/listings/ListingCategory.tsx
+++ app/components/listings/ListingCategory.tsx
@@ -10,12 +10,24 @@ interface ListingCategoryProps {
 }
 
 const ListingCategory: React.FC<ListingCategoryProps> = ({
-	icon,
+	icon: Icon,
 	label,
 	description
 }) => {
 	return ( 
-		<div></div>
+		<div className="flex flex-col gap-6">
+			<div className="flex flex-row items-center gap-4">
+				<Icon size={40} className="text-neutral-600" />
+				<div className="flex flex-col">
+					<div className="text-lg font-semibold">
+						{label}
+					</div>
+					<div className="text-neutral-500 font-light">
+						{description}
+					</div>
+				</div>
+			</div>
+		</div>
 	);
 }
 
