57acc599183a43e8c39d2f19c7e9deb3a4e21c5b
Kyaw Thu <evanch98@gmail.com>
2023-05-18 21:16:10 +0800

    add new parameters

--- app/components/listings/ListingCategory.tsx
+++ app/components/listings/ListingCategory.tsx
@@ -1,8 +1,19 @@
 "use client";
 
 import React from "react";
+import { IconType } from "react-icons";
 
-const ListingCategory = () => {
+interface ListingCategoryProps {
+  icon: IconType;
+  label: string;
+  description: string
+}
+
+const ListingCategory: React.FC<ListingCategoryProps> = ({
+	icon,
+	label,
+	description
+}) => {
 	return ( 
 		<div></div>
 	);
