a35578fcf7c49591c252c8f924ee407e9f21840d
Kyaw Thu <evanch98@gmail.com>
2023-05-18 20:07:35 +0800

    add new parameters to the ListingHead

--- app/components/listings/ListingHead.tsx
+++ app/components/listings/ListingHead.tsx
@@ -1,6 +1,23 @@
+"use client";
+
+import { SafeUser } from "@/app/types";
 import React from "react";
 
-const ListingHead = () => {
+interface ListingHeadProps {
+  title: string;
+  imageSrc: string;
+  locationValue: string;
+  id: string;
+  currentUser?: SafeUser | null;
+}
+
+const ListingHead: React.FC<ListingHeadProps> = ({
+	title,
+	imageSrc,
+	locationValue,
+	id,
+	currentUser,
+}) => {
 	return ( 
 		<div>Listing Head</div>
 	);
