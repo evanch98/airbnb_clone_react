c62ce6f46937045e5ee25801d7ad3057cc872288
Kyaw Thu <evanch98@gmail.com>
2023-05-18 20:15:15 +0800

    add contents to the ListingHead component

--- app/components/listings/ListingHead.tsx
+++ app/components/listings/ListingHead.tsx
@@ -1,7 +1,11 @@
 "use client";
 
+import useCountries from "@/app/hooks/useCountry";
 import { SafeUser } from "@/app/types";
 import React from "react";
+import Heading from "../Heading";
+import Image from "next/image";
+import HeartButton from "../HeartButton";
 
 interface ListingHeadProps {
   title: string;
@@ -18,8 +22,39 @@ const ListingHead: React.FC<ListingHeadProps> = ({
 	id,
 	currentUser,
 }) => {
+	const { getByValue } = useCountries();
+
+	const location = getByValue(locationValue);
+
 	return ( 
-		<div>Listing Head</div>
+		<>
+			<Heading 
+				title={title}
+				subtitle={`${location?.region}, ${location?.label}`}
+			/>
+			<div
+				className="
+          w-full
+          h-[60vh]
+          overflow-hidden
+          rounded-xl
+          relative
+        "
+			>
+				<Image 
+					alt="Image"
+					src={imageSrc}
+					fill
+					className="object-cover w-full"
+				/>
+				<div className="absolute top-5 right-5">
+					<HeartButton 
+						listingId={id}
+						currentUser={currentUser}
+					/>
+				</div>
+			</div>
+		</>
 	);
 }
 
