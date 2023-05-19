4a2fd1b50164ebb19cb3526c79ce4569417924ef
Kyaw Thu <evanch98@gmail.com>
2023-05-19 21:14:32 +0800

    add an async function PropertiesPage

---
+++ app/properties/page.tsx
@@ -0,0 +1,46 @@
+import getCurrentUser from "../actions/getCurrentUser";
+import getListings from "../actions/getListings";
+import EmptyState from "../components/EmptyState";
+import ClientOnly from "../components/navbar/ClientOnly";
+import React from "react";
+
+const PropertiesPage = async () => {
+	const currentUser = await getCurrentUser();
+
+	if (!currentUser) {
+		return (
+			<ClientOnly>
+				<EmptyState
+					title="Unauthorized"
+					subtitle="Please login"
+				/>
+			</ClientOnly>
+		);
+	}
+
+	const listings = await getListings({
+		userId: currentUser.id,
+	});
+
+	if (listings.length === 0) {
+		return (
+			<ClientOnly>
+				<EmptyState
+					title="No properties found"
+					subtitle="Looks like you have no properties listed."
+				/>
+			</ClientOnly>
+		);
+	}
+
+	return (
+		<ClientOnly>
+			<PropertiesClient 
+				listings={listings}
+				currentUser={currentUser}
+			/>
+		</ClientOnly>
+	);
+}
+
+export default PropertiesPage;
