5a1a2d8bccd1d7129e097adfce1827d5b7f411ba
Kyaw Thu <evanch98@gmail.com>
2023-05-19 21:56:07 +0800

    add SearchModal

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import ToasterProvider from "./providers/ToasterProvider";
 import LoginModal from "./components/modals/LoginModal";
 import getCurrentUser from "./actions/getCurrentUser";
 import RentModal from "./components/modals/RentModal";
+import SearchModal from "./components/modals/SearchModal";
 
 const font = Nunito({ subsets: ["latin"] });
 
@@ -30,6 +31,7 @@ export default async function RootLayout({
 					<RegisterModal />
 					<LoginModal />
 					<RentModal />
+					<SearchModal />
 					<Navbar currentUser={currentUser} />
 				</ClientOnly>
 				<div className="pb-20 pt-28">{children}</div>
