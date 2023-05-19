aad0ed8b279b15553e8264b1e575810f5e6067b1
Kyaw Thu <evanch98@gmail.com>
2023-05-19 22:17:53 +0800

    add onBack, onNext, and onSubmit function

--- app/components/modals/SearchModal.tsx
+++ app/components/modals/SearchModal.tsx
@@ -2,8 +2,13 @@
 
 import useSearchModal from "@/app/hooks/useSearchModal";
 import Modal from "./Modal";
-import React, { useState } from "react";
+import React, { useCallback, useMemo, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
+import { Range } from "react-date-range";
+import dynamic from "next/dynamic";
+import { CountrySelectValue } from "../inputs/CountrySelect";
+import qs from "query-string";
+import { formatISO } from "date-fns";
 
 enum STEPS {
   LOCATION = 0,
@@ -16,7 +21,77 @@ const SearchModal = () => {
 	const params = useSearchParams();
 	const searchModal = useSearchModal();
 
+	const [location, setLocation] = useState<CountrySelectValue>();
 	const [step, setStep] = useState(STEPS.LOCATION);
+	const [guestCount, setGuestCount] = useState(1);
+	const [roomCount, setRoomCount] = useState(1);
+	const [bathroomCount, setBathroomCount] = useState(1);
+	const [dateRange, setDateRange] = useState<Range>({
+		startDate: new Date(),
+		endDate: new Date(),
+		key: "selection"
+	});
+
+	const Map = useMemo(() => dynamic(() => import("../Map"), {
+		ssr: false,
+	}), [location]);
+
+	const onBack = useCallback(() => {
+		setStep((value) => value - 1);
+	}, [setStep]);
+
+	const onNext = useCallback(() => {
+		setStep((value) => value + 1);
+	}, [setStep]);
+
+	const onSubmit = useCallback(async () => {
+		if (step !== STEPS.INFO) {
+			onNext();
+		}
+
+		let currentQuery = {};
+
+		if (params) {
+			currentQuery = qs.parse(params.toString());
+		}
+
+		const updatedQuery: any = {
+			...currentQuery,
+			locationValue: location?.value,
+			guestCount,
+			roomCount,
+			bathroomCount,
+		};
+
+		if (dateRange.startDate) {
+			updatedQuery.startDate = formatISO(dateRange.startDate);
+		}
+
+		if (dateRange.endDate) {
+			updatedQuery.endDate = formatISO(dateRange.endDate);
+		}
+
+		const url = qs.stringifyUrl({
+			url: "/",
+			query: updatedQuery
+		}, { skipNull: true });
+
+		setStep(STEPS.LOCATION);
+		searchModal.onClose();
+		router.push(url);
+	}, [
+		step,
+		searchModal,
+		location,
+		router,
+		guestCount,
+		roomCount,
+		bathroomCount,
+		dateRange,
+		onNext,
+		params
+	]);
+
 	return ( 
 		<Modal
 			isOpen={searchModal.isOpen}
