800bd8160dc95d1630866bba86525833417e42bf
Kyaw Thu <evanch98@gmail.com>
2023-05-19 17:17:07 +0800

    add a logic to navigate to the trips page

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -9,6 +9,7 @@ import useLoginModal from "@/app/hooks/useLoginModal";
 import { signOut } from "next-auth/react";
 import { SafeUser } from "@/app/types";
 import useRentModal from "@/app/hooks/useRentModal";
+import { useRouter } from "next/navigation";
 
 interface UserMenuProps {
 	currentUser?: SafeUser | null;
@@ -19,6 +20,7 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
 	const registerModal = useRegisterModal();
 	const loginModal = useLoginModal();
 	const rentModal = useRentModal();
+	const router = useRouter();
 
 	const [isOpen, setIsOpen] = useState(false);
 
@@ -101,7 +103,7 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
 						{currentUser ? (
 							// if the user has logged in
 							<>
-								<MenuItem onClick={() => {}} label="My trips" />
+								<MenuItem onClick={() => router.push("/trips")} label="My trips" />
 								<MenuItem onClick={() => {}} label="My favorites" />
 								<MenuItem onClick={() => {}} label="My reservations" />
 								<MenuItem onClick={() => {}} label="My properties" />
