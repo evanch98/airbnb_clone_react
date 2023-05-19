4eb183fed06cf685a50439941d23a7b92d8c9c78
Kyaw Thu <evanch98@gmail.com>
2023-05-19 11:49:23 +0800

    add new parameters

--- app/components/inputs/Calendar.tsx
+++ app/components/inputs/Calendar.tsx
@@ -1,6 +1,19 @@
-import React from "react";
+"use client";
 
-const Calendar = () => {
+import React from "react";
+import { Range, RangeKeyDict } from "react-date-range";
+
+interface CalendarProps {
+  value: Range;
+  onChange: (value: RangeKeyDict) => void;
+  disabledDates?: Date[];
+}
+
+const Calendar: React.FC<CalendarProps> = ({
+	value,
+	onChange,
+	disabledDates
+}) => {
 	return ( 
 		<div></div>
 	);
