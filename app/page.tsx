d98b5220ff4395a10c79c1aa1a8db9ba7688bdfb
Kyaw Thu <evanch98@gmail.com>
2023-05-20 00:47:33 +0800

    force dynamic

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,8 @@ import getListings, { IListingParams } from "./actions/getListings";
 import ListingCard from "./components/listings/ListingCard";
 import getCurrentUser from "./actions/getCurrentUser";
 
+export const dynamic = "force-dynamic";
+
 interface HomeProps {
 	searchParams: IListingParams;
 }
