43ce3ef5f53fba9de46aef751241a9c28275515e
Kyaw Thu <evanch98@gmail.com>
2023-05-19 21:59:21 +0800

    add enum STEPS and necessary constants

--- app/components/modals/SearchModal.tsx
+++ app/components/modals/SearchModal.tsx
@@ -2,10 +2,21 @@
 
 import useSearchModal from "@/app/hooks/useSearchModal";
 import Modal from "./Modal";
-import React from "react";
+import React, { useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+
+enum STEPS {
+  LOCATION = 0,
+  DATE = 1,
+  INFO = 2,
+}
 
 const SearchModal = () => {
+	const router = useRouter();
+	const params = useSearchParams();
 	const searchModal = useSearchModal();
+
+	const [step, setStep] = useState(STEPS.LOCATION);
 	return ( 
 		<Modal
 			isOpen={searchModal.isOpen}
