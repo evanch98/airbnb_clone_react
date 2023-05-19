2a181d8a1f57e741b93d6df57089a047232ddc53
Kyaw Thu <evanch98@gmail.com>
2023-05-19 18:27:38 +0800

    change the type of reservation from Reservation to SafeReservation

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -1,8 +1,7 @@
 "use client";
 
 import useCountries from "@/app/hooks/useCountry";
-import { SafeListing, SafeUser } from "@/app/types";
-import { Reservation } from "@prisma/client";
+import { SafeListing, SafeReservation, SafeUser } from "@/app/types";
 import { format } from "date-fns";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
@@ -12,7 +11,7 @@ import Button from "../Button";
 
 interface ListingCardProps {
 	data: SafeListing;
-	reservation?: Reservation;
+	reservation?: SafeReservation;
 	onAction?: (id: string) => void;
 	disabled?: boolean;
 	actionLabel?: string;
