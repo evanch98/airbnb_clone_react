cf3c267ada28e5b9e467c3b67030ce9d894e302a
Kyaw Thu <evanch98@gmail.com>
2023-05-19 11:46:00 +0800

    add contents

--- app/components/listings/ListingReservation.tsx
+++ app/components/listings/ListingReservation.tsx
@@ -23,7 +23,38 @@ const ListingReservation: React.FC<ListingReservationProps> = ({
 	disabledDates,
 }) => {
 	return ( 
-		<div></div>
+		<div
+			className="
+        bg-white
+        rounded-xl
+        border-[1px]
+        border-neutral-200
+        overflow-hidden
+      "
+		>
+			<div
+				className="
+          flex
+          flex-row
+          items-center
+          gap-1
+          p-4
+        "
+			>
+				<div className="text-2xl font-semibold">
+          $ {price}
+				</div>
+				<div className="font-light text-neutral-600">
+          night
+				</div>
+			</div>
+			<hr />
+			<Calendar 
+				value={dateRange}
+				disabledDates={disabledDates}
+				onChange={(value) => onChangeDate(value.selection)}
+			/>
+		</div>
 	);
 }
 
