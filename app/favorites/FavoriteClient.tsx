6a72f7622cb7996e392a3d32459758c7ee212b97
Kyaw Thu <evanch98@gmail.com>
2023-05-19 20:57:00 +0800

    add contents

--- app/favorites/FavoriteClient.tsx
+++ app/favorites/FavoriteClient.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import { SafeListing, SafeUser } from "../types";
 import Container from "../components/Container";
 import Heading from "../components/Heading";
+import ListingCard from "../components/listings/ListingCard";
 
 interface FavoriteClientProps {
   listings: SafeListing[];
@@ -20,6 +21,27 @@ const FavoriteClient: React.FC<FavoriteClientProps> = ({
 				title="Favorites"
 				subtitle="List of places you have favorited!"
 			/>
+			<div
+				className="
+          mt-10
+          grid
+          grid-cols-1
+          sm:grid-cols-2
+          md:grid-cols-3
+          lg:grid-cols-4
+          xl:grid-cols-5
+          2xl:grid-cols-6
+          gap-8
+        "
+			>
+				{listings.map((listing) => (
+					<ListingCard
+						key={listing.id}
+						currentUser={currentUser}
+						data={listing}
+					/>
+				))}
+			</div>
 		</Container>
 	);
 }
