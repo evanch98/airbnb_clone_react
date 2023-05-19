c7dc967f86d2defedc9b2c551f9e10d833a4d121
Kyaw Thu <evanch98@gmail.com>
2023-05-19 18:14:00 +0800

    add contents and onCancel function

--- app/trips/TripsClient.tsx
+++ app/trips/TripsClient.tsx
@@ -1,5 +1,12 @@
-import React from "react";
+"use client";
+
+import React, { useCallback, useState } from "react";
 import { SafeReservation, SafeUser } from "../types";
+import Container from "../components/Container";
+import Heading from "../components/Heading";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+import { toast } from "react-hot-toast";
 
 interface TripsClientProps {
   reservations: SafeReservation[];
@@ -10,8 +17,44 @@ const TripsClient: React.FC<TripsClientProps> = ({
 	reservations,
 	currentUser,
 }) => {
+	const router = useRouter();
+	const [deletingId, setDeletingId] = useState("");
+
+	const onCancel = useCallback((id: string) => {
+		setDeletingId(id);
+
+		axios.delete(`/api/reservations/${id}`)
+			.then(() => {
+				toast.success("Reservation cancelled");
+				router.refresh();
+			})
+			.catch((error) => {
+				toast.error(error?.response?.data?.error);
+			})
+			.finally(() => {
+				setDeletingId("");
+			});
+	}, [setDeletingId]);
+
 	return ( 
-		<div>My trips</div>
+		<Container>
+			<Heading 
+				title="Trips"
+				subtitle="Where you've been and where you're going"
+			/>
+			<div
+				className="
+          mt-10
+          grid
+          grid-cols-1
+          sm:grid-cols-2
+          md:grid-cols-3
+          lg:grid-cols-4
+          xl:grid-cols-5
+          2xl:grid-cols-6
+        "
+			></div>
+		</Container>
 	);
 }
 
