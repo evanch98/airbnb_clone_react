17cd63b7496aa5a5007745a548fe6c1c42fb16b6
Kyaw Thu <evanch98@gmail.com>
2023-05-19 20:30:25 +0800

    add contents

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -1,14 +1,40 @@
 import EmptyState from "../components/EmptyState";
 import ClientOnly from "../components/navbar/ClientOnly";
 import getCurrentUser from "../actions/getCurrentUser";
+import getFavoriteListings from "../actions/getFavoriteListings";
 import React from "react";
 
 const ListingPage = async () => {
+	const listings = await getFavoriteListings();
+	const currentUser = await getCurrentUser();
+
+	if (!currentUser) {
+		return (
+			<ClientOnly>
+				<EmptyState
+					title="Unauthorized"
+					subtitle="Please login"
+				/>
+			</ClientOnly>
+		);
+	}
+
+	if (listings.length === 0) {
+		return (
+			<ClientOnly>
+				<EmptyState
+					title="No favorites found"
+					subtitle="Looks like you have no favorite listings."
+				/>
+			</ClientOnly>
+		);
+	}
+
 	return (
 		<ClientOnly>
-			<EmptyState 
-				title="No favorites found"
-				subtitle="Looks like you have no favorite listings."
+			<FavoriteClient
+				listings={listings}
+				currentUser={currentUser}
 			/>
 		</ClientOnly>
 	);
