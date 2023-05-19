1d26246e9cdbf5ba86bb6def79f9e803098d0783
Kyaw Thu <evanch98@gmail.com>
2023-05-19 20:21:20 +0800

    add a new function ListingPage

---
+++ app/favorites/page.tsx
@@ -0,0 +1,17 @@
+import EmptyState from "../components/EmptyState";
+import ClientOnly from "../components/navbar/ClientOnly";
+import getCurrentUser from "../actions/getCurrentUser";
+import React from "react";
+
+const ListingPage = async () => {
+	return (
+		<ClientOnly>
+			<EmptyState 
+				title="No favorites found"
+				subtitle="Looks like you have no favorite listings."
+			/>
+		</ClientOnly>
+	);
+}
+
+export default ListingPage;
