dcace4998cec05a4159c2b3f7d507fb80b848d13
Kyaw Thu <evanch98@gmail.com>
2023-05-19 17:11:18 +0800

    add an async function TripsPage

--- app/trips/page.tsx
+++ app/trips/page.tsx
@@ -2,6 +2,44 @@ import EmptyState from "../components/EmptyState";
 import ClientOnly from "../components/navbar/ClientOnly";
 import getCurrentUser from "../actions/getCurrentUser";
 import getReservations from "../actions/getReservations";
+import React from "react";
 
 
+const TripsPage = async () => {
+	const currentUser = await getCurrentUser();
 
+	if (!currentUser) {
+		return (
+			<ClientOnly>
+				<EmptyState 
+					title="Unauthorized"
+					subtitle="Please login"
+				/>
+			</ClientOnly>
+		);
+	}
+
+	const reservations = await getReservations({
+		userId: currentUser.id,
+	});
+
+	if (reservations.length === 0) {
+		return (
+			<ClientOnly>
+				<EmptyState 
+					title="No trips found"
+					subtitle="Looks like you haven't reserved any trips."
+				/>
+			</ClientOnly>
+		);
+	}
+
+	return (
+		<ClientOnly>
+			<TripsClient 
+				reservations={reservations}
+				currentUser={currentUser}
+			/>
+		</ClientOnly>
+	);
+}
