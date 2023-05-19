78a1bf07b0cde210f105dcb54ce78cf540f55701
Kyaw Thu <evanch98@gmail.com>
2023-05-19 19:42:30 +0800

    add contents to the ReservationClient

--- app/reservations/ReservationClient.tsx
+++ app/reservations/ReservationClient.tsx
@@ -43,6 +43,32 @@ const ReservationClient: React.FC<ReservationClientProps> = ({
 				title="Reservations"
 				subtitle="Bookings on your properties"
 			/>
+			<div
+				className="
+          mt-10
+          grid
+          grid-cols-1
+          sm:grid-cols-2
+          md:grid-cols-3
+          lg:grid-cols-4
+          xl:grid-cols-5
+          2xl:grid-cols-6
+          gap-8
+        "
+			>
+				{reservations.map((reservation) => (
+					<ListingCard
+						key={reservation.id}
+						data={reservation.listing}
+						reservation={reservation}
+						actionId={reservation.id}
+						onAction={onCancel}
+						disabled={deletingId === reservation.id}
+						actionLabel="Cancel guest reservation"
+						currentUser={currentUser}
+					/>
+				))}
+			</div>
 		</Container>
 	);
 }
