286c30092a5534d7d614ca705106a186494e41e5
Kyaw Thu <evanch98@gmail.com>
2023-05-19 17:05:45 +0800

    import necessary components

---
+++ app/trips/page.tsx
@@ -0,0 +1,7 @@
+import EmptyState from "../components/EmptyState";
+import ClientOnly from "../components/navbar/ClientOnly";
+import getCurrentUser from "../actions/getCurrentUser";
+import getReservations from "../actions/getReservations";
+
+
+
