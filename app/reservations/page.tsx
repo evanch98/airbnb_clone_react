9508381e932818c98439cbac4dc53555c4ac31c7
Kyaw Thu <evanch98@gmail.com>
2023-05-19 19:23:52 +0800

    import the ReservationClient

--- app/reservations/page.tsx
+++ app/reservations/page.tsx
@@ -3,6 +3,7 @@ import ClientOnly from "../components/navbar/ClientOnly";
 import getCurrentUser from "../actions/getCurrentUser";
 import getReservations from "../actions/getReservations";
 import React from "react";
+import ReservationClient from "./ReservationClient";
 
 const ReservationPage = async () => {
 	const currentUser = await getCurrentUser();
