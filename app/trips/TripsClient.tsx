0d5d0361ade0c8ed743ae9c048e5c972fefff586
Kyaw Thu <evanch98@gmail.com>
2023-05-19 18:27:54 +0800

    add ListingCard

--- app/trips/TripsClient.tsx
+++ app/trips/TripsClient.tsx
@@ -7,6 +7,7 @@ import Heading from "../components/Heading";
 import { useRouter } from "next/navigation";
 import axios from "axios";
 import { toast } from "react-hot-toast";
+import ListingCard from "../components/listings/ListingCard";
 
 interface TripsClientProps {
   reservations: SafeReservation[];
@@ -52,8 +53,22 @@ const TripsClient: React.FC<TripsClientProps> = ({
           lg:grid-cols-4
           xl:grid-cols-5
           2xl:grid-cols-6
+          gap-8
         "
-			></div>
+			>
+				{reservations.map((reservation) => (
+					<ListingCard 
+						key={reservation.id}
+						data={reservation.listing}
+						reservation={reservation}
+						actionId={reservation.id}
+						onAction={onCancel}
+						disabled={deletingId === reservation.id}
+						actionLabel="Cancel reservation"
+						currentUser={currentUser}
+					/>
+				))}
+			</div>
 		</Container>
 	);
 }
