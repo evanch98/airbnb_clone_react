c7742b1641ed0640304457a8a1f630b52b9d3d05
Kyaw Thu <evanch98@gmail.com>
2023-05-19 22:56:24 +0800

    add a logic to filter items

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -15,13 +15,68 @@ export default async function getListings(
 	params: IListingParams,
 ) {
 	try {
-		const { userId } = params;
+		const { 
+			userId,
+			guestCount,
+			roomCount,
+			bathroomCount,
+			startDate,
+			endDate,
+			locationValue,
+			category
+		} = params;
 
 		const query: any = {};
 
 		if (userId) {
 			query.userId = userId;
 		}
+
+		if (category) {
+			query.category = category;
+		}
+
+		if (roomCount) {
+			query.roomCount = {
+				gte: +roomCount,
+			}
+		}
+
+		if (bathroomCount) {
+			query.bathroomCount = {
+				gte: +bathroomCount,
+			}
+		}
+
+		if (guestCount) {
+			query.guestCount ={
+				gte: +guestCount,
+			}
+		}
+
+		if (locationValue) {
+			query.locationValue = locationValue
+		}
+
+		if (startDate && endDate) {
+			query.NOT = {
+				reservations: {
+					some: {
+						OR: [
+							{
+								endDate: { gte: startDate },
+								startDate: { lte: startDate },
+							},
+							{
+								startDate: { lte: endDate },
+								endDate: { gte: endDate },
+							},
+						],
+					},
+				},
+			}
+		}
+
 		// retrieve listings from the server
 		const listings = await prisma.listing.findMany({
 			where: query,
