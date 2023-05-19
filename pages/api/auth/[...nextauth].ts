7b6741e1e19913f32a02f8da6a0f561d6b75435f
Kyaw Thu <evanch98@gmail.com>
2023-05-20 01:57:40 +0800

    minor change

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -3,7 +3,7 @@ import NextAuth, { AuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 
 import prisma from "@/app/libs/prismadb";
 
