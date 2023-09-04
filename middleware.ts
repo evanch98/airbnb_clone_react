4a6bd317edfce5f066be508fdf5de36266a7d5ad
Kyaw Thu <evanch98@gmail.com>
2023-09-05 00:11:52 +0800

    reformat the code

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,5 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-	matcher: [
-		"/trips",
-		"/reservations",
-		"/properties",
-		"/favorites"
-	],
-}
+  matcher: ["/trips", "/reservations", "/properties", "/favorites"],
+};
