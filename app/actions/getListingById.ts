c83a93c65b9aab51279f85b76e3dfbca12d4463e
Kyaw Thu <evanch98@gmail.com>
2023-09-05 00:04:38 +0800

    reformat the code

--- app/actions/getListingById.ts
+++ app/actions/getListingById.ts
@@ -6,33 +6,33 @@ interface IParams {
 
 // retrieve the listing that matches the given id from the server
 export default async function getListingById(params: IParams) {
-	try {
-		const { listingId } = params;
+  try {
+    const { listingId } = params;
 
-		const listing = await prisma.listing.findUnique({
-			where: {
-				id: listingId
-			},
-			include: {
-				user: true
-			}
-		});
+    const listing = await prisma.listing.findUnique({
+      where: {
+        id: listingId,
+      },
+      include: {
+        user: true,
+      },
+    });
 
-		if (!listing) {
-			return null;
-		}
+    if (!listing) {
+      return null;
+    }
 
-		return {
-			...listing,
-			createdAt: listing.createdAt.toISOString(),
-			user: {
-				...listing.user,
-				createdAt: listing.user.createdAt.toISOString(),
-				updatedAt: listing.user.updatedAt.toISOString(),
-				emailVerified: listing.user.emailVerified?.toISOString() || null,
-			}
-		}
-	} catch (error: any) {
-		throw new Error(error);
-	}
+    return {
+      ...listing,
+      createdAt: listing.createdAt.toISOString(),
+      user: {
+        ...listing.user,
+        createdAt: listing.user.createdAt.toISOString(),
+        updatedAt: listing.user.updatedAt.toISOString(),
+        emailVerified: listing.user.emailVerified?.toISOString() || null,
+      },
+    };
+  } catch (error: any) {
+    throw new Error(error);
+  }
 }
