df11a98208012920c1232b130a39273bc19cc6f6
Kyaw Thu <evanch98@gmail.com>
2023-09-05 00:04:25 +0800

    reformat the code

--- app/actions/getFavoriteListings.ts
+++ app/actions/getFavoriteListings.ts
@@ -2,28 +2,28 @@ import prisma from "@/app/libs/prismadb";
 import getCurrentUser from "./getCurrentUser";
 
 export default async function getFavoriteListings() {
-	try {
-		const currentUser = await getCurrentUser();
+  try {
+    const currentUser = await getCurrentUser();
 
-		if (!currentUser) {
-			return [];
-		}
+    if (!currentUser) {
+      return [];
+    }
 
-		const favorites = await prisma.listing.findMany({
-			where: {
-				id: {
-					in: [...(currentUser.favoriteIds || [])],
-				},
-			},
-		});
+    const favorites = await prisma.listing.findMany({
+      where: {
+        id: {
+          in: [...(currentUser.favoriteIds || [])],
+        },
+      },
+    });
 
-		const safeFavorites = favorites.map((favorite) => ({
-			...favorite,
-			createdAt: favorite.createdAt.toISOString(),
-		}));
+    const safeFavorites = favorites.map((favorite) => ({
+      ...favorite,
+      createdAt: favorite.createdAt.toISOString(),
+    }));
 
-		return safeFavorites;
-	} catch (error: any) {
-		throw new Error(error);
-	}
+    return safeFavorites;
+  } catch (error: any) {
+    throw new Error(error);
+  }
 }
